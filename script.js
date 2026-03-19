71da25b55d92d8d3e09464c065787e60d7fd3abc
Samuel Silva <samuelpds77@gmail.com>
2026-03-19 06:30:08 -0300

    CARD TO-DO-LIST TERMINADO

--- script.js
+++ script.js
@@ -16,42 +16,156 @@ setInterval(() => {
   atualizarHora();
 }, 1000);
 
-
 //ARRAY PARA GUARDAR TODASAS TAREFAS
-const tarefasArray = []
+const tarefasArray = [];
+
+//FUNÇAO PARA MANIPULAR O DOM
+function enviarHTML(tarefa, classe) {
+  Tarefas.innerHTML += `
+        <div class="${classe}" id="${tarefa}" onclick="mudarClasse('${tarefa}')">
+          <p> ${tarefa} </p>
+          <button onclick="deletar('${tarefa}')">Deletar</button>
+        </div>
+  `;
+}
 
 //ADICIONAR TAREFA
 
-let Tarefas = document.querySelector(".tarefas")
-let inputAdd = document.getElementById("inputAdd")
+let Tarefas = document.querySelector(".tarefas");
+let inputAdd = document.getElementById("inputAdd");
 
-function adicionarTarefa(){
-  let campoInput = inputAdd.value
-  let caixaBaixa = campoInput.toLowerCase()
+function adicionarTarefa() {
+  let campoInput = inputAdd.value;
+  let caixaBaixa = campoInput.toLowerCase();
 
   //VERIFICAR SE JA TEM UMA TAREFA COM O MESMO NOME
   const nomeTarefa = tarefasArray.some((valor) => {
-    return valor == caixaBaixa
-  })
+    return valor.tarefa == caixaBaixa;
+  });
 
-  if(inputAdd.value !== "" && nomeTarefa == false){
-  Tarefas.innerHTML += `
-        <div class="tarefa" id="${caixaBaixa}">
-          <input type="checkbox" >
-          <p> ${caixaBaixa} </p>
-          <button onclick="deletar('${caixaBaixa}')">Deletar</button>
-        </div>
-  `
-  tarefasArray.push(caixaBaixa)
-  inputAdd.value = ""
-  inputAdd.focus()
+  if (inputAdd.value !== "" && nomeTarefa == false) {
+    //FUNÇAO QUE MUDA O HTML
+    enviarHTML(caixaBaixa, "tarefa");
+
+    tarefasArray.push({ tarefa: caixaBaixa, completado: false });
+    console.log(tarefasArray);
+    inputAdd.value = "";
+    inputAdd.focus();
+
+    //LEMBRAR DA TAREFA
+    let informaçoes = JSON.stringify({
+      tarefa: caixaBaixa,
+      completado: false,
+    });
+    localStorage.setItem(caixaBaixa, informaçoes);
   }
+}
 
+inputAdd.addEventListener("keyup",(valor) => {
+  if (valor.keyCode === 13) {
+    adicionarTarefa()
+  }
+});
+
+//MUDAR CLASSE (MARCAR COMO CONCLUIDO)
+function mudarClasse(idTarefa) {
+  let element = document.getElementById(idTarefa);
+  localStorage.removeItem(idTarefa);
+
+  let posiçao = "";
+  tarefasArray.forEach((index, ordem) => {
+    if (index.tarefa == idTarefa) {
+      posiçao = ordem;
+    }
+  });
+
+  if (element.classList.contains("tarefa")) {
+    element.classList.remove("tarefa");
+    element.classList.add("tarefaC");
+    tarefasArray[posiçao].completado = true;
+    localStorage.setItem(
+      idTarefa,
+      JSON.stringify({ tarefa: idTarefa, completado: true }),
+    );
+  } else {
+    element.classList.remove("tarefaC");
+    element.classList.add("tarefa");
+    tarefasArray[posiçao].completado = false;
+    localStorage.setItem(
+      idTarefa,
+      JSON.stringify({ tarefa: idTarefa, completado: false }),
+    );
+  }
 }
 
 //DELETAR TAREFA
 
-function deletar(valor){
-  let element = document.getElementById(valor)
-  element.remove()
+function deletar(valor) {
+  let elemento = document.getElementById(valor);
+  elemento.remove();
+  localStorage.removeItem(valor);
+
+  //DELETAR DO ARRAY
+  let posicao = "";
+  tarefasArray.forEach((index, ordem) => {
+    if (index.tarefa == valor) {
+      posicao = ordem;
+    }
+  });
+  tarefasArray.splice(posicao, 1);
+  console.log(tarefasArray);
+}
+
+//LEMBRAR TAREFA
+
+for (let i = 0; i < localStorage.length; i++) {
+  let ordem = localStorage.key(i);
+  let info = JSON.parse(localStorage.getItem(ordem));
+
+  tarefasArray.push({ tarefa: info.tarefa, completado: info.completado });
+  console.log(info.completado);
+  if (info.completado == true) {
+    //FUNÇAO QUE MUDA O HTML
+    enviarHTML(info.tarefa, "tarefaC");
+  } else {
+    //FUNÇAO QUE MUDA O HTML
+    enviarHTML(info.tarefa, "tarefa");
+  }
+}
+
+//FILTRAGEM
+function todas() {
+  tarefasArray.forEach((valor) => {
+    let element = document.getElementById(valor.tarefa);
+    element.style.display = "flex";
+  });
+}
+
+//FILTRAR POR TAREFAS PENDENTES
+
+function pendente() {
+  tarefasArray.forEach((valor) => {
+    if (valor.completado == true) {
+      let element = document.getElementById(valor.tarefa);
+      element.style.display = "none";
+    } else {
+      let element = document.getElementById(valor.tarefa);
+
+      element.style.display = "flex";
+    }
+  });
+}
+
+//FILTRAR POR TAREFAS CONCLUIDAS
+
+function concluidas() {
+  tarefasArray.forEach((valor) => {
+    if (valor.completado == false) {
+      let element = document.getElementById(valor.tarefa);
+      element.style.display = "none";
+    } else {
+      let element = document.getElementById(valor.tarefa);
+      element.style.display = "flex";
+    }
+  });
 }
